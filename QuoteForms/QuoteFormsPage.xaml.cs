a8c6abca1d3c95f2a7d92f2a94eb828c9012c6d4
raja95shah <raja95shah@gmail.com>
2016-10-05 23:31:33 +0800

    initial commit

---
+++ QuoteForms/QuoteFormsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace QuoteForms
+{
+    public partial class QuoteFormsPage : ContentPage
+    {
+        public QuoteFormsPage()
+        {
+            InitializeComponent();
+
+            var quoteViewModel = new QuoteViewModel();
+            var label = new Label();
+            label.SetBinding(Label.TextProperty, "QuoteName");
+            this.OnChildAdded(label);
+
+            BindingContext = quoteViewModel;
+        }
+    }
+}
---
+++ QuoteForms/ViewModels/QuoteViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace QuoteForms
+{
+    public class QuoteViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public QuoteViewModel()
+        {
+            QuoteName = "First Name";
+        }
+
+        string quoteName;
+        public string QuoteName
+        {
+            get { return quoteName;}
+            set
+            {
+                quoteName = value;
+                if(PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("QuoteName"));
+                }
+            }
+        }
+
+        public Command ResetQuoteName
+        {
+            get 
+            {
+                return new Command (() =>
+                {
+                  QuoteName = "First Value";  
+                }
+                                    );
+            }
+        }
+    }
+}
